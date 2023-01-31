4e77630740332871f34296654072dec3bb9d6945
Konrad-Jalowy <sitesystem@wp.pl>
2023-01-31 11:38:45 +0100

    display seeded tasks

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Task;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+ 
+    return view('welcome', [
+        'tasks' => $tasks
+    ]);
 });
